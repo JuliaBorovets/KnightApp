16befe02451a27ce6705026dab6e5030efeae333
JuliaBorovets <juliaborovets2001@gmail.com>
2020-02-17 18:24:46 +0200

    View changed. TextConstant added.

---
+++ src/main/java/view/TextConstant.java
@@ -0,0 +1,9 @@
+package main.java.view;
+
+public interface TextConstant {
+    String INPUT_STRING_DATA = "input.string.data";
+    String INPUT_LANGUAGE = "input.string.language";
+    String INPUT_MIN = "input.string.writeMin";
+    String INPUT_MAX = "input.string.writeMax";
+    String INPUT_ERROR = "input.string.error";
+}
